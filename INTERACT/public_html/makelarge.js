6f7c14eccaefdcd91ec478c5d235ce03b4f85a7c
Frida Pettersen <35762523+Gnusifus@users.noreply.github.com>
2019-03-04 15:23:36 +0100

    Opprydning

--- INTERACT/public_html/makelarge.js
+++ INTERACT/public_html/makelarge.js
@@ -1,22 +1,29 @@
 $(function(){
-
     $(".node").mouseenter(function(){
+        //fjerner hover effekt på tomme nodes.
         if($(this).find(".node").hasClass("empty")){
-        $(".node:hover").css("transform", "none");
-    };
-        $(this).find(".edit_icons").css("display", "block");
+          $(".node:hover").css("transform", "none");
+        }
+        //Viser edit_icons hvis .node ikke er tom ved mouseenter.
+        else{
+          $(this).find(".edit_icons").css("display", "block");
+        };
     }).mouseleave(function(){
+        //Skjuler .edit_icons ved mouseleave
         $(this).find(".edit_icons").css("display", "none");
 
+        //gjør card-title ikke-editable ved mouseleave
         if($(this).find(".card-title").hasClass("editable")){
             $(".card-title").removeClass("editable");
         }
+        //fjerner .display_block fra noden sin .new_picture div.
         if($(this).find(".new_picture").hasClass("display_block")){
            $(".new_picture").removeClass("display_block");
         };
     });
 
     $(".main-menu").hover(function(){
+      //Setter farger på hvert ikon i valgmeny ved hover.
       $(".teksticon").css("color", "white");
       $(".bildeicon").css("color", "#FF5C44");
       $(".videoicon").css("color", "#FF72D3");
@@ -25,6 +32,7 @@ $(function(){
       $(".linkicon").css("color", "#1AD44A");
       $(".oppgaveicon").css("color", "#FF961A");
     }, function(){
+      //Tilbakestiller fargen til alle ikoner i valgmeny, når ikke hover.
       $("li").find("i").css("color", "gray");
     })
 
