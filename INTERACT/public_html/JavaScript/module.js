cbfc2fb9f39a5b76bb26bb70934349a0cf7e7b59
Frida Pettersen <35762523+Gnusifus@users.noreply.github.com>
2019-04-02 15:02:21 +0200

    Bilde validering

--- INTERACT/public_html/JavaScript/module.js
+++ INTERACT/public_html/JavaScript/module.js
@@ -1,15 +1,45 @@
-//Teller chars i textaera
-function countChar(val) {
-  var len = val.value.length;
-  if (len >= 500) {
-    val.value = val.value.substring(0, 500);
-  } else {
-    $('#teller').text(500 - len);
-  }
-};
+  //Teller chars i textaera
+  function countChar(val) {
+    var len = val.value.length;
+    if (len >= 500) {
+      val.value = val.value.substring(0, 500);
+    } else {
+      $('#teller').text(500 - len);
+    }
+  };
 
-//Viser tekst i label for bilde når bilde er lastet opp
-$('.custom-file-input').on('change', function() {
-   let fileName = $(this).val().split('\\').pop();
-   $(this).next('.custom-file-label').addClass("selected").html(fileName);
+  $(function(){
+
+  //Viser tekst i label for bilde når bilde er lastet opp
+  $('.custom-file-input').on('change', function() {
+     let fileName = $(this).val().split('\\').pop();
+     $(this).next('.custom-file-label').addClass("selected").html(fileName);
+  });
+
+  //Sjekker bilde input
+  $('.image-input').on('change', function(){
+    var file = this.files[0];
+    console.log(file);
+    var fileType = file["type"];
+    console.log(fileType); //image/jpeg
+    var validImageTypes = ["image/gif", "image/jpeg", "image/png"];
+    if ($.inArray(fileType, validImageTypes) < 0) {
+       $(".feil").addClass("d-block");
+       $(".feil").removeClass("d-none");
+     }
+   else{
+     $(".feil").removeClass("d-block");
+     $(".feil").addClass("d-none");
+   }
+  });
+
 });
+
+function checkimg(){
+  if($(".feil").hasClass("d-block")){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
--- INTERACT/public_html/PHP/show_all_cases.php
+++ INTERACT/public_html/PHP/show_all_cases.php
@@ -4,9 +4,8 @@ include 'dbconnect.php';
 
 $sql="SELECT * FROM cases";
 $result = mysqli_query($conn,$sql);
-$row = mysqli_fetch_array($result);
 
-if($_SESSION['loggetinn'] == true && $row != 0){
+if($_SESSION['loggetinn'] == true && mysqli_num_rows($result) >= 0){
   while($row = mysqli_fetch_array($result)) {
     $dbdate = DateTime::createFromFormat('Y-m-d H:i:s', $row['dato']);
     $nordate = $dbdate->format('d/m/Y');
@@ -71,7 +70,7 @@ if($_SESSION['loggetinn'] == true && $row != 0){
     </div>
     ";
 }
-elseif($_SESSION['loggetinn'] == true && $row == 0){ //Hvis ingen caser er laget
+elseif($_SESSION['loggetinn'] == true && mysqli_num_rows($result) == 0){ //Hvis ingen caser er laget
   echo "
   <div class='col-lg-3 col-md-4 col-sm-6 portfolio-item'>
     <span data-toggle='tooltip' data-placement='right' title='Du har ingen caser enda, klikk her for å begynne!'>
